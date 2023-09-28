9f4845f7f54d094e341021c3ee73ab30212a1158
jbouwman7 <jbouwman7@st.noorderpoort.nl>
2023-09-28 11:41:06 +0200

    woord genereren

---
+++ word.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+
+$words = [
+    "Tijgerprint",
+    "Paraplu",
+    "Toverstaf",
+    "Bloemenveld",
+    "Krokodil",
+    "Watermeloen",
+    "Zonsopgang",
+    "Gitaarmuziek",
+    "Tropisch",
+    "Kastanjebruin",
+    "Puzzelstukje",
+    "Fietspad",
+    "Avontuurlijk",
+    "Schaduwrijk",
+    "Hoogtepunt",
+    "Sneeuwvlok",
+    "Oceaanbries",
+    "Appelboom",
+    "Woestijnzand",
+    "Regenboog",
+    "Vogelnest",
+    "Donderwolk",
+    "Picknick",
+    "Schommelstoel",
+    "Zonneschijn",
+    "Vuurwerk",
+    "Herfstblad",
+    "Wintersport",
+    "Tropenhitte",
+    "Zonnebloem"
+];
+$randomWord = $words[array_rand($words)];
+
+if (!empty($_POST)) {
+    if (isset($_POST['word'])) {
+        $_SESSION['chosenWord'] = $_POST['word'];
+    }
+
+    if (isset($_POST['randomWord'])) {
+        $_SESSION['chosenWord'] = $randomWord;
+    }
+
+    header('location:galgje.php');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/output.css">
+    <title>Glagje</title>
+</head>
+
+<body class="bg-gradient-to-r from-amber-400 to-amber-700">
+    <div class="mx-auto w-fit bg-black rounded text-white bg-opacity-30 px-5 py-3 mt-10">
+        <h1 class="text-center font-bold text-4xl">Galgje</h1>
+        <h2 class="text-center font-semibold text-3xl py-4">Kies een eigen woord of krijg een willekeurige</h2>
+        <div class="flex justify-between">
+            <form action="#" method="POST">
+                <label for="word"></label>
+                <input type="text" id="word" name="word" class="rounded text-black px-1" required>
+                <input type="submit" class="bg-white text-amber-950 px-2 py-0.5 rounded" value="Eigen woord">
+            </form>
+            <form action="#" method="POST">
+                <input type="hidden" name="randomWord">
+                <input type="submit" class="bg-white text-amber-950 px-2 py-0.5 rounded" value="Willekeurig woord">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
