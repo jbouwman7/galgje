74d34386050fb40cbe26f2366974fb3ef4515079
jbouwman7 <jbouwman7@st.noorderpoort.nl>
2023-09-29 09:41:28 +0200

    final push

--- galgje.php
+++ galgje.php
@@ -1,4 +1,6 @@
 <?php
+
+require_once 'functions.php';
 session_start();
 if (isset($_POST['restart'])) {
     session_destroy();
@@ -13,20 +15,10 @@ if (!isset($_SESSION['chosenWord'])) {
 
 if (!isset($_SESSION['wrongCount'])) $_SESSION['wrongCount'] = 0;
 if (!isset($_SESSION['availableLetters'])) $_SESSION['availableLetters'] = range('A', 'Z');
-$_SESSION['arrWord'] = str_split($_SESSION["chosenWord"]);
-$key = count($_SESSION["arrWord"]);
 if (isset($_POST['chosenLetter'])) $_SESSION['chosenLetter'] = $_POST['chosenLetter'];
 
-$correctLetters = [];
-$i = 0;
-if (isset($_SESSION['chosenLetter'])) {
-    foreach ($_SESSION['arrWord'] as $letter) {
-        $i++;
-        if ($letter == $_SESSION['chosenLetter']) {
-            $correctLetters[$i] = $_SESSION['chosenLetter'];
-        }
-    }
-}
+$_SESSION['arrWord'] = str_split($_SESSION["chosenWord"]);
+$key = count($_SESSION["arrWord"]);
 
 checkLetter();
 
@@ -37,33 +29,17 @@ if (!isset($_SESSION["guessedArray"])) {
     $_SESSION["guessedArray"] = $guessedArray;
 }
 
-var_dump($_SESSION);
-function delLetterFromArrWord()
-{
-    $key = array_search($_SESSION["chosenLetter"], $_SESSION['availableLetters']);
-    unset($_SESSION['availableLetters'][$key]);
-    $_SESSION['availableLetters'] = array_values($_SESSION['availableLetters']);
+if (array_search('-', $_SESSION['guessedArray']) === false) {
+    $winLose = TRUE;
 }
 
-function checkLetter()
-{
-    if (isset($_SESSION['chosenLetter'])) {
-        if (in_array($_SESSION["chosenLetter"], $_SESSION["arrWord"])) {
-            correctLetter();
-        } else {
-            $_SESSION['wrongCount']++;
-        }
-        delLetterFromArrWord();
-    }
+if ($_SESSION['wrongCount'] >= 7) {
+    $winLose = FALSE;
 }
 
-function correctLetter()
-{
-    $replace = array_keys($_SESSION["arrWord"], $_SESSION["chosenLetter"]);
-    foreach ($replace as $key) {
-        $_SESSION["guessedArray"][$key] = $_SESSION["chosenLetter"];
-    }
-}
+
+$_SESSION['kansen'] = 7 - $_SESSION['wrongCount'];
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -77,9 +53,27 @@ function correctLetter()
 
 <body class="bg-gradient-to-r from-amber-400 to-amber-700">
     <div class="mx-auto w-fit">
-        <?php foreach ($_SESSION['guessedArray'] as $letter) : ?>
-            <p><?= $letter ?></p>
-        <?php endforeach ?>
+        <?php if (isset($winLose)) : ?>
+            <div class="text-center my-5">
+                <?php if ($winLose) : ?>
+                    <h1 class="font-bold text-5xl">Gefeliciteerd!</h1>
+                    <h2 class="text-3xl">U heeft het juiste woord geraden</h2>
+                <?php else : ?>
+                    <h1 class="font-bold text-5xl">U heeft verloren</h1>
+                    <h2 class="text-3xl">Het juiste woord was: <span class="underline"><?= $_SESSION['chosenWord'] ?></span></h2>
+                <?php endif ?>
+            </div>
+        <?php endif ?>
+        <?php if (!isset($winLose)) : ?>
+            <div class="text-center mt-3">
+                <p>U heeft nog: <span class="undelrine"><?= $_SESSION['kansen'] ?></span?> kansen</p>
+            </div>
+        <?php endif ?>
+        <div class="flex gap-x-1 w-fit mx-auto mt-3">
+            <?php foreach ($_SESSION['guessedArray'] as $letter) : ?>
+                <p><?= $letter ?></p>
+            <?php endforeach ?>
+        </div>
         <form action="" method="post" class="flex gap-x-3 w-fit">
             <?php foreach ($_SESSION['availableLetters'] as $key => $aLetter) : ?>
                 <input type="submit" name="chosenLetter" value="<?= $aLetter ?>" id="<?= $aLetter ?>" class="hover:cursor-pointer hover:underline font-bold">
